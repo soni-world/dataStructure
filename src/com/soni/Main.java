04d0bc8ba57a220ca826f84e920ed528a913e076
sonicercli <soni@cercli.com>
2023-07-04 16:24:03 +0400

    initial commit

---
+++ src/com/soni/Main.java
@@ -0,0 +1,8 @@
+package com.soni;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/soni/Tree.java
@@ -0,0 +1,4 @@
+package com.soni;
+
+public class Tree {
+}
